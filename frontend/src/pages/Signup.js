93a713383acb63131e91e8e2bda35fea2d40a1ac
elenabli <elena.blinova@metropolia.fi>
2023-12-01 10:25:07 +0200

    signup and useSighup, useField

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,15 +1,30 @@
+import { useSignup } from "../hooks/useSignup";
+import {useField} from "../hooks/useField";
+
 const Signup = () => {
+  const name = useField("text");
+  const email = useField("email");
+  const password = useField("password");
+
+  const { signup, error, isLoading } = useSignup();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(email.value, password.value);
+  };
+
   return (
-    <form className="signup">
+    <form className="signup" onSubmit={handleSubmit}>
       <h3>Sign Up</h3>
       <label>Name:</label>
-      <input type="text" />
+      <input {...name} />
       <label>Email address:</label>
-      <input type="email" />
+      <input {...email} />
       <label>Password:</label>
-      <input type="password" />
+      <input {...password} />
 
-      <button>Sign up</button>
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
