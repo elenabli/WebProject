ccdc23b4c848d687ac175912a5966d4bdbc28c15
elenabli <elena.blinova@metropolia.fi>
2023-12-01 10:55:25 +0200

    useField to goalForm, little fix in useLogin

--- frontend/src/components/GoalForm.js
+++ frontend/src/components/GoalForm.js
@@ -1,10 +1,14 @@
+import {useField} from "../hooks/useField";
+
 const GoalForm = () => {
+  const goal = useField("text");
+
   return (
     <form className="create">
       <h3>Add a New Goal</h3>
 
       <label>Text:</label>
-      <input type="text" className="" />
+      <input {...goal} className="" />
       <button>Add Goal</button>
     </form>
   );
