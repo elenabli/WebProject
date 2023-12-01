986475e2c4a1218eb7aef73d9c643fe282e048d8
valtat <82716180+valtat@users.noreply.github.com>
2023-12-01 15:32:14 +0200

    Added authentication and error middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,7 +1,12 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
 
-}
+  res.status(500);
+  res.json({
+    message: error.message,
+  });
+};
 
 module.exports = {
   errorHandler,
-}
+};
