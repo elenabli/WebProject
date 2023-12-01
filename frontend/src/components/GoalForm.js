6b1072962f17093979b4629bd76610161721ca4e
JKK <jonne.karhunen@metropolia.fi>
2023-12-01 10:52:48 +0200

    useFetch

--- frontend/src/components/GoalForm.js
+++ frontend/src/components/GoalForm.js
@@ -1,10 +1,27 @@
-const GoalForm = () => {
-  return (
-    <form className="create">
-      <h3>Add a New Goal</h3>
+import React, { useState } from "react";
+import useFetch from "../hooks/useFetch";
 
+const GoalForm = () => {
+  const [goal, setGoal] = useState("");
+  const { fetchData } = useFetch("https://api.example.com/goals", "POST");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { text: goal };
+    await fetchData(data);
+    setGoal("");
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Goal</h3>
       <label>Text:</label>
-      <input type="text" className="" />
+      <input
+        type="text"
+        className=""
+        value={goal}
+        onChange={(e) => setGoal(e.target.value)}
+      />
       <button>Add Goal</button>
     </form>
   );
