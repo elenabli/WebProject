6fd28b8fe6a0ea4be560709914a17a1387268e16
valtat <82716180+valtat@users.noreply.github.com>
2023-12-01 11:49:33 +0200

    Added goalController and userController

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,70 @@
-const Goal = require('../models/goalModel');
-const User = require('../models/userModel');
+const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = async (req, res, next) => {
+  const goals = await Goal.find({ user: req.user._id });
 
+  res.status(200).json(goals);
 };
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
 const setGoal = async (req, res, next) => {
+  const { text, user } = req.body;
 
+  try {
+    const goal = await Goal.create({ text, user });
+
+    res.status(200).json(goal);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = async (req, res, next) => {
- 
+  const { text, user } = req.body;
+
+  try {
+    const goal = await Goal.findById(req.params.id);
+
+    if (goal) {
+      goal.text = text /* || goal.text */;
+      goal.user = user /* || goal.user */;
+
+      const updatedGoal = await goal.save();
+
+      res.status(200).json(updatedGoal);
+    } else {
+      res.status(404).json({ message: "Goal not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = async (req, res, next) => {
+  const goal = await Goal.findById(req.params.id);
 
+  try {
+    if (goal) {
+      await goal.remove();
+      res.status(200).json({ message: "Goal removed" });
+    } else {
+      res.status(404).json({ message: "Goal not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = {
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,56 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/userModel');
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = async (req, res, next) => {
- 
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.registerUser(name, email, password);
+
+    const token = createToken(user._id);
+    const id = user._id;
+
+    res.status(200).json({ id, name, email, token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
 
+  try {
+    const user = await User.loginUser(email, password);
+
+    const token = createToken(user._id);
+    const id = user._id;
+
+    res.status(200).json({ id, email, token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
 const getMe = async (req, res, next) => {
- 
-};
+  const user = await User.findById(req.user._id).select("-password");
 
+  res.status(200).json(user);
+};
 
 module.exports = {
   registerUser,
