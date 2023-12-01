a4ea3563ff0cac73bc4b84aca69b3357fe824854
valtat <82716180+valtat@users.noreply.github.com>
2023-12-01 11:48:24 +0200

    Added goalModel and userModel

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,7 +1,54 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const goalSchema = mongoose.Schema(
-  
-)
+const Schema = mongoose.Schema;
 
-module.exports = mongoose.model('Goal', goalSchema)
+const goalSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+goalSchema.statics.getGoals = async function (user) {
+  const goals = await this.find({ user });
+
+  return goals;
+};
+
+goalSchema.statics.setGoal = async function (text, user) {
+  const goal = await this.create({ text, user });
+
+  return goal;
+};
+
+goalSchema.statics.updateGoal = async function (id, text, user) {
+  const goal = await this.findById(id);
+
+  if (goal) {
+    goal.text = text /* || goal.text */;
+    goal.user = user /* || goal.user */;
+
+    const updatedGoal = await goal.save();
+
+    return updatedGoal;
+  } else {
+    throw new Error("Goal not found");
+  }
+};
+
+goalSchema.statics.deleteGoal = async function (id) {
+  const goal = await this.findById(id);
+
+  if (goal) {
+    await goal.remove();
+  } else {
+    throw new Error("Goal not found");
+  }
+};
+
+module.exports = mongoose.model("Goal", goalSchema);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,73 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
-const userSchema = mongoose.Schema(
-  
-)
+const Schema = mongoose.Schema;
 
-module.exports = mongoose.model('User', userSchema)
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.registerUser = async function (name, email, password) {
+  if (!name || !email || !password) {
+    throw new Error("Please provide name, email and password");
+  }
+  if (!validator.isEmail(email)) {
+    throw new Error("Please provide a valid email address");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Please provide a strong password");
+  }
+  const existingUser = await this.findOne({ email });
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    name,
+    email,
+    password: hash,
+  });
+
+  return user;
+};
+
+userSchema.statics.loginUser = async function (email, password) {
+  if (!email || !password) {
+    throw new Error("Please provide email and password");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid credentials");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid credentials");
+  }
+
+  return user;
+};
+
+userSchema.statics.getMe = async function (id) {
+  const user = await this.findById(id).select("-password");
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
